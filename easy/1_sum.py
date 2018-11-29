fa2fbb3ede26cf4234c95904c0ea638783e700fc
yingfeng <yingfeng@imagedt.com>
2018-11-29 16:45:56 +0800

    首次刷题，迈出第一脚

---
+++ easy/1_sum.py
@@ -0,0 +1,15 @@
+"""
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的 两个 整数。
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+"""
+
+
+class Solution:
+    def two_sum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+
