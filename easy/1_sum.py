9d345e2bd5f9431e2f3347748baae95f05842db1
yingfeng <yingfeng@imagedt.com>
2018-11-29 17:31:19 +0800

    第一题结束

--- easy/1_sum.py
+++ easy/1_sum.py
@@ -7,9 +7,44 @@
 class Solution:
     def two_sum(self, nums, target):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+        第一次提交，提示超时 T_T
         """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                count = nums[i] + nums[j]
+                if count == target:
+                    return [i, j]
+
+    def two_sum2(self, nums, target):
+        """
+        第二次提交, 1368ms
+        """
+        for i in range(len(nums)):
+            flag = target - nums[i]
+            if flag in nums:
+                sec = nums.index(flag, 0, len(nums))
+                if i == sec:
+                    continue
+                return [i, sec]
+
+    def two_sum3(self, nums, target):
+        """
+        第三次提交，希望有好点的结果，哈哈哈哈 48ms
+        """
+        dic = dict(zip(nums, range(len(nums))))
+
+        for i in range(len(nums)):
+            flag = target - nums[i]
+            if flag in dic and i != dic[flag]:
+                return [i, dic[flag]]
 
 
+    def run(self):
+        nums = [2, 7, 11, 15]
+        target = 9
+        num = self.two_sum3(nums, target)
+        print(num)
+
+
+if __name__ == '__main__':
+    Solution().run()
